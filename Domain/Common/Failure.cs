b529808080f82c529db4e5bb47a46f9b01f24ab0
Sam Blake <sam_blake94@hotmail.co.uk>
2023-06-22 08:53:41 +0100

    Added Result classes

---
+++ Domain/Common/Failure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.Common
+{
+    public class Failure<T>
+        : Result<T>
+    {
+        public Failure(Exception exception)
+        {
+            _exception = exception;
+        }
+
+        public new T Value { get => default; }
+        public new Exception Exception { get => _exception; }
+        public override bool IsSuccess { get => false; }
+    }
+}
---
+++ Domain/Common/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.Common
+{
+    public abstract class Result<T>
+    {
+        protected T _value;
+        protected Exception _exception;
+
+        public T Value { get; set; }
+        public abstract bool IsSuccess { get; }
+        public Exception Exception { get; set; }
+    }
+}
